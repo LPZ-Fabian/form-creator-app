426707155b12fe5da2aaacd2825606119325e471
BrandonC123 <brandoc318@gmail.com>
2022-04-01 14:36:40 -0700

    Reroute "/" path to manage forms page.

--- form-creator-app-frontend/src/App.js
+++ form-creator-app-frontend/src/App.js
@@ -1,35 +1,72 @@
 import "./App.css";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from "react-router-dom";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import ListUserFormElementsComponent from "./components/ListUserFormElementsComponent";
 import ListDefaultFormElementsComponent from "./components/ListDefaultFormElementsComponent";
-//import AddUserFormElementComponent from "./components/AddUserFormElementComponent";
 import UpdateUserElementComponent from "./components/UpdateUserElementComponent";
 import AddUserElementComponent from "./components/AddUserElementComponent";
 import ManageFormComponent from "./components/ManageFormComponent";
 import CreateFormComponent from "./components/CreateFormComponents";
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <HeaderComponent />
-        <div className="container">
-          <Routes>
-          <Route exact path = "/manage-forms/" element = {<ManageFormComponent/>}></Route>
-            <Route path = "/add-element/:id" element = {<ListDefaultFormElementsComponent/>}></Route>
-            <Route path = "/update-form-element/:id" element = {<UpdateUserElementComponent/>}></Route>
-            <Route path = "/build-element/:defaultId/:formId" element = {<AddUserElementComponent/>}></Route>
-            <Route path = "/update-form/:id" element = {<ListUserFormElementsComponent/>}></Route>
-            <Route path = "/user-form/:id" element = {<ListUserFormElementsComponent/>}></Route>
-            <Route path = "/build-form/" element = {<CreateFormComponent/>}></Route>
-          </Routes>
+    const redirect = true;
+    return (
+        <div>
+            <Router>
+                <HeaderComponent />
+                <div className="container">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                redirect ? (
+                                    <Navigate replace to="/manage-forms" />
+                                ) : (
+                                    <ManageFormComponent />
+                                )
+                            }
+                        ></Route>
+                        <Route
+                            exact
+                            path="/manage-forms/"
+                            element={<ManageFormComponent />}
+                        ></Route>
+                        <Route
+                            path="/add-element/:id"
+                            element={<ListDefaultFormElementsComponent />}
+                        ></Route>
+                        <Route
+                            path="/update-form-element/:id"
+                            element={<UpdateUserElementComponent />}
+                        ></Route>
+                        <Route
+                            path="/build-element/:defaultId/:formId"
+                            element={<AddUserElementComponent />}
+                        ></Route>
+                        <Route
+                            path="/update-form/:id"
+                            element={<ListUserFormElementsComponent />}
+                        ></Route>
+                        <Route
+                            path="/user-form/:id"
+                            element={<ListUserFormElementsComponent />}
+                        ></Route>
+                        <Route
+                            path="/build-form/"
+                            element={<CreateFormComponent />}
+                        ></Route>
+                    </Routes>
+                </div>
+                <FooterComponent />
+            </Router>
         </div>
-        <FooterComponent />
-      </Router>
-    </div>
-  );
+    );
 }
 
 export default App;
--- form-creator-app-frontend/src/components/HeaderComponent.js
+++ form-creator-app-frontend/src/components/HeaderComponent.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const HeaderComponent = () => {
-  return (
-    <div>
-        <header>
-            <nav className = "navbar-expand-md navbar-dark bg-dark">
-                <div>
-                    <a href = "https://CtrlAltDefeat.net" className = "navbar-brand">
-                        Form Creator Application
-                    </a>
-                </div>
+    return (
+        <div>
+            <header>
+                <nav className="navbar-expand-md navbar-dark bg-dark">
+                    <div>
+                        <a
+                            href="http://localhost:3000/manage-forms"
+                            className="navbar-brand"
+                        >
+                            Form Creator Application
+                        </a>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    );
+};
 
-            </nav>
-        </header>
-    </div>
-  )
-}
-
-export default HeaderComponent
+export default HeaderComponent;
